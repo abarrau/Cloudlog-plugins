8b3eeed1776d47542cd6fa6cfcda8ac7136536f0
abarrau <hesky378@gmail.com>
2023-11-02 18:34:25 +0100

    Delete Cloudlog-add-files/application/third_party_pluginsext/examplemod directory

--- Cloudlog-add-files/application/third_party_pluginsext/examplemod/views/examplemod/myTestFunction.php
+++
@@ -1,6 +0,0 @@
-<div class="logdata">
-    <div class="table-responsive">
-        Hello, thanks for use Cloudlog and add other function.<br/>
-        You answer of the question is : <? echo $q1; ?>
-    </div>
-</div>
